aad132b2c04715bf71c47f42f7c788673bd8797e
simone-05 <simone9903@gmail.com>
2022-09-09 17:30:30 +0200

    mod

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 docker-compose up -d
+
+docker-compose exec mongo "mongoimport --db iot --collection test --jsonArray /home/input_docs-archives/device_measure_1000.json"
+
+docker-compose exec mongo "mongoimport --db grafo-db --collection graphs /home/input_docs-archives/example_graph.json"
