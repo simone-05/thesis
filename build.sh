31f905a3d7231547f54fbd051a2d495473abfb6a
simone-05 <simone9903@gmail.com>
2022-09-09 18:44:57 +0200

    mods

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-chown -R $USER ./grafana/data
-chmod -R 777 ./grafana/data
-
 cd ./ang/grafo1 && npm install && ng b
 
 cd ../../spring-db/grafo && \
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker-compose up -d
+docker-compose up -d && \
 
-docker-compose exec mongo "mongoimport --db iot --collection test --jsonArray /home/input_docs-archives/device_measure_1000.json"
+docker-compose exec mongo mongoimport --db iot --collection test /home/input_docs-archives/device_measure_1000.json 2> /dev/null && \
 
-docker-compose exec mongo "mongoimport --db grafo-db --collection graphs /home/input_docs-archives/example_graph.json"
+docker-compose exec mongo mongoimport --db grafo-db --collection graphs /home/input_docs-archives/example_graph.json 2> /dev/null
